feaeb76f7b68eb8b667afdfc083d27426f3113fc
Paul <gazzillo@cs.yale.edu>
2017-12-22 14:17:11 -0500

    script to generated dimacs output from the .kmax output via --dimacs

---
+++ kconfig/dimacs.py
@@ -0,0 +1,255 @@
+import os
+import sys
+import compiler
+import ast
+
+# this script takes the check_dep --dimacs output and converts it into
+# a dimacs-compatible format
+
+root_var = "SPECIAL_ROOT_VARIABLE"
+
+varnums = {}
+varnums[root_var] = 1
+
+nonbools = set()
+nonbools_with_dep = set()
+defaultvals = {}
+
+def lookup_varnum(varname):
+  if varname not in varnums:
+    varnums[varname] = len(varnums) + 1
+  return varnums[varname]
+
+class Transformer(compiler.visitor.ASTVisitor):
+  def __init__(self):
+    compiler.visitor.ASTVisitor.__init__(self)
+    self.tree = ()
+
+  def visitDiscard(self, node):
+    self.tree = self.visit(node.getChildren()[0])
+
+  def visitOr(self, node):
+    children = node.getChildren()
+    left = self.visit(children[0])
+    for i in range(1, len(children)):
+      left = ("or", left, self.visit(children[i]))
+    return left
+
+  def visitAnd(self, node):
+    children = node.getChildren()
+    left = self.visit(children[0])
+    for i in range(1, len(children)):
+      left = ("and", left, self.visit(children[i]))
+    return left
+
+  def visitNot(self, node):
+    return ("not", self.visit(node.expr))
+
+  def visitName(self, node):
+    return ("name", node.name)
+
+  def visitConst(self, node):
+    return ("const", int(node.value))
+
+def convert(node):
+  """takes a tree and returns a list of clauses or a constant value"""
+  nodetype = node[0]
+  if nodetype == "or":
+    left = node[1]
+    right = node[2]
+    l = convert(left)
+    r = convert(right)
+
+    # check for constants
+    if isinstance(l, int):
+      if l == 0:
+        return r
+      else:
+        return 1
+    if isinstance(r, int):
+      if r == 0:
+        return l
+      else:
+        return 1
+
+    assert(isinstance(l, list) and isinstance(r, list))
+    # construct new clauses by taking each pairwise combination of l and r's elements
+    new_clauses = []
+    for lc in l:
+      for rc in r:
+        assert(isinstance(lc, list) and isinstance(rc, list))
+        new_clauses.append(lc + rc)
+    return new_clauses
+
+  elif nodetype == "and":
+    left = node[1]
+    right = node[2]
+    l = convert(left)
+    r = convert(right)
+
+    # check for constants
+    if isinstance(l, int):
+      if l == 0:
+        return 0
+      else:
+        return r
+    if isinstance(r, int):
+      if r == 0:
+        return 0
+      else:
+        return l
+      
+    assert(isinstance(l, list) and isinstance(r, list))
+    return l + r  # join two lists of clauses
+
+  elif nodetype == "not":
+    negated_node = node[1]
+    negated_type = negated_node[0]
+    if negated_type == "name":
+      varnum = lookup_varnum(negated_node[1])
+      return [[-varnum]]
+    if negated_type == "const":
+      value = negated_node[1]
+      if value == 0:
+        return 1
+      else:
+        return 0
+    elif negated_type == "not":
+      return convert(negated_node[1]) # double-negation
+    elif negated_type == "and":
+      left = negated_node[1]
+      right = negated_node[2]
+      return convert(("or", ("not", left), ("not", right))) # de morgan
+    elif negated_type == "or":
+      left = negated_node[1]
+      right = negated_node[2]
+      return convert(("and", ("not", left), ("not", right))) # de morgan
+    else:
+      assert(False)
+
+  elif nodetype == "name":
+    varnum = lookup_varnum(node[1])
+    return [[varnum]]
+
+  elif nodetype == "const":
+    return node[1]
+
+  else:
+    assert(False)
+
+def convert_to_cnf(expr):
+  ast = compiler.parse(expr)
+  # get Discard(ACTUAL_EXPR) from Module(None, Stmt([Discard(ACTUAL_EXPR)))
+  actual_expr = ast.getChildNodes()[0].getChildNodes()[0]
+  # print ast
+  transformer = Transformer()
+  compiler.walk(actual_expr, transformer)
+  tree = transformer.tree
+  # print tree
+  clauses = convert(tree)
+  if (isinstance(clauses, list)):
+    return clauses
+  else:
+    # constants don't add any clauses
+    return []
+
+# print convert_to_cnf("not a or (b and (c or d)) and not (e and f)")
+# print convert_to_cnf("not a or (b and (c or d)) and not (e and f)")
+# print convert_to_cnf("not a or (b and (c or d)) and not (e and f)")
+# print convert_to_cnf("not a or (b and (c or d)) and not (e and f)")
+# print convert_to_cnf("not a or (b and (c or d)) and not (e and f) and g or h")
+# print convert_to_cnf("a or b or c or d or f")
+# exit(1)
+
+clauses = []
+clauses.append([lookup_varnum(root_var)])
+for line in sys.stdin:
+  instr, data = line.strip().split(" ", 1)
+  if (instr == "bool"):
+    tokens = data.split(" ", 1)
+    varname = tokens[0]
+    lookup_varnum(varname)
+    if len(tokens) > 1:
+      defaultvals[varname] = tokens[1]
+  elif (instr == "nonbool"):
+    tokens = data.split(" ", 1)
+    varname = tokens[0]
+    lookup_varnum(varname)
+    nonbools.add(varname)
+    if len(tokens) > 1:
+      defaultvals[varname] = tokens[1]
+  elif (instr == "clause"):
+    vars = data.split(" ")
+    clause = []
+    for var in vars:
+      if (var[0] == "-"):  # negation
+        realvar = var[1:]
+        clause.append(-lookup_varnum(realvar))
+      else:
+        clause.append(lookup_varnum(var))
+    clauses.append(clause)
+  elif (instr == "bool_choice"):
+    var_string, dep_expr = data.split("|",1)
+    config_vars = var_string.split(" ")
+    # print config_vars
+    # mutex choice: a -> !b, a -> !c, ..., b -> !a, b -> !c, ...
+    for i in range(0, len(config_vars)):
+      for j in range(0, len(config_vars)):
+        if i != j:
+          var_i = lookup_varnum(config_vars[i])
+          var_j = lookup_varnum(config_vars[j])
+          clause = [-var_i, -var_j]
+          # print clause
+          clauses.append(clause)
+    # add dependency, (!dep | a | b | ... ), i.e., dep -> one of a,b,... this also ensures at least one is selected
+    or_vars = ""
+    for var in config_vars:
+      or_vars = or_vars + " or " + var
+    if dep_expr == "(1)":
+      dep_expr = root_var
+    else:
+      dep_expr = dep_expr
+    choice_dep = "((not %s)%s)" % (dep_expr, or_vars)
+    # print choice_dep
+    new_clauses = convert_to_cnf(choice_dep)
+    # print new_clauses
+    clauses.extend(new_clauses)
+  elif (instr == "dep"):
+    # print instr,data
+    var, expr = data.split(" ", 1)
+    # # if no dependencies, then depend on special root variable
+    # if expr == "(1)": expr = root_var
+    # if no dependencies, don't add any clause
+    if expr != "(1)":
+      # var -> expr, i.e., not var or expr
+      full_expr = "(not (" + var + ")) or (" + expr + ")"
+      # print full_expr
+      new_clauses = convert_to_cnf(full_expr)
+      # print new_clauses
+      clauses.extend(new_clauses)
+      if var in nonbools:
+        # nonbools are mandatory unless disabled by dependency, so we
+        # also ensure that nonbool var is selected whenever its
+        # dependencies holds
+        full_expr = "(not (" + expr + ")) or (" + var + ")"
+        new_clauses = convert_to_cnf(full_expr)
+        # print new_clauses
+        clauses.extend(new_clauses)
+        nonbools_with_dep.add(var)
+  else:
+    sys.stderr.write("unsupported instruction: %s\n" % (line))
+    exit(1)
+
+for nonbool in (nonbools - nonbools_with_dep):
+  varnum = lookup_varnum(nonbool)
+  rootnum = lookup_varnum(root_var)
+  clauses.append([-varnum, rootnum])
+  clauses.append([varnum, -rootnum])
+  
+for varname in sorted(varnums, key=varnums.get):
+  vartype = "nonbool" if varname in nonbools else "bool"
+  defaultval = " " + defaultvals[varname] if varname in defaultvals else ""
+  print "c %d %s %s%s" % (varnums[varname], varname, vartype, defaultval)
+print "p cnf %d %d" % (len(varnums), len(clauses))
+for clause in clauses:
+  print "%s 0" % (" ".join([str(num) for num in clause]))
