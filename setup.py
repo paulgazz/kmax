4086016594db5d4aa04fc7d5078dedcc1f46b472
Paul <github@pgazz.com>
2019-11-24 20:38:32 -0500

    factored out klocalization functions into localize.py
    this leaves klocalizer and klocalizerlinux as cli front-ends

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
     ],
-    scripts=['kmaxtools/kmax', 'kmaxtools/kmaxall', 'kmaxtools/kclause', 'kmaxtools/klocalizer'],
+    scripts=['kmaxtools/kmax', 'kmaxtools/kmaxall', 'kmaxtools/kclause', 'kmaxtools/klocalizer', 'kmaxtools/klocalizerlinux'],
     install_requires=[
         'enum34',
         'regex',
