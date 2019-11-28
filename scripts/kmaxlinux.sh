b68bfcd1ed75964fb71ab7b1f8690e65596cfc8c
Paul <github@pgazz.com>
2019-11-28 03:13:59 -0500

    save arch-specific directories separately

--- scripts/kmaxlinux.sh
+++ scripts/kmaxlinux.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -x
+
 script_dir=$(dirname $0)
 makefile_override="$script_dir/makefile_override"
+mkdir -p .kmax/topleveldirs
 for arch in $(ls arch/ | grep -v Kconfig); do
-  timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null
-done | tr ' ' '\n' | sort | uniq > .kmax/topleveldirs
+  timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null > .kmax/topleveldirs/$arch
+done
 
-/usr/bin/time kmaxall -z $(cat .kmax/topleveldirs) > .kmax/kmax
+/usr/bin/time kmaxall -z $(cat .kmax/topleveldirs/* | tr ' ' '\n' | sort | uniq) > .kmax/kmax
