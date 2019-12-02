5b066fe6ca44dd39f6ed9a83675f1aa0e0dcbdfd
Paul <github@pgazz.com>
2019-12-02 01:34:46 -0500

    add support for collecting subdirs from arch-specific Makefiles
    core-y, core-m, etc setup top-level directories inside of the
    arch/NAME/Makefile of each arch.  now kmax will collect directories
    from these kbuild variables and kmaxlinux.sh will setup the
    per-architecture directories to start searching accordingly.  kmaxall
    is updated to emit warnings to stderr instead of stdout, which break
    pickling.

--- scripts/kmaxlinux.sh
+++ scripts/kmaxlinux.sh
@@ -5,8 +5,8 @@ set -x
 script_dir=$(dirname $0)
 makefile_override="$script_dir/makefile_override"
 mkdir -p .kmax/topleveldirs
-for arch in $(ls arch/ | grep -v Kconfig); do
-  timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null > .kmax/topleveldirs/$arch
+for arch in $(ls arch/ | egrep -v ".gitignore|Kconfig"); do
+  echo -n "arch/$arch " > .kmax/topleveldirs/$arch
+  timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null >> .kmax/topleveldirs/$arch
 done
-
 /usr/bin/time kmaxall -z $(cat .kmax/topleveldirs/* | tr ' ' '\n' | sort | uniq) > .kmax/kmax
