b303d9fb09f308c7aa6c2d8a0b9cbefde4e1e265
Paul <github@pgazz.com>
2019-12-06 00:50:51 -0500

    added script to convert kclause formula into a dimacs file
    this uses z3's built-in functionality and has not been tested.

---
+++ scripts/kclause_to_dimacs.py
@@ -0,0 +1,34 @@
+import pickle
+import z3
+
+def get_kclause_constraints(kclause_file):
+  with open(kclause_file, 'r') as fp:
+    # kclause, defined_vars, used_vars = pickle.load(fp)
+    kclause = pickle.load(fp)
+
+    kclause_constraints = {}
+    for var in kclause.keys():
+      kclause_constraints[var] = [ z3.parse_smt2_string(clause) for clause in kclause[var] ]
+
+    constraints = []
+    for var in kclause_constraints.keys():
+      for z3_clause in kclause_constraints[var]:
+        constraints.extend(z3_clause)
+
+    return constraints
+
+constraints = get_kclause_constraints(".kmax/kclause/x86_64/kclause")
+
+solver = z3.Solver()
+
+use_tseitin = True
+
+if use_tseitin:
+  g = z3.Goal()
+  g.add(constraints)
+  t = z3.Tactic('tseitin-cnf')
+  solver.add(g)
+else:
+  solver.add(constraints)
+
+print(solver.dimacs())
