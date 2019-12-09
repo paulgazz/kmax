d855d28bd8e44e3272cabaac5cefffa87a4e46f8
Paul <github@pgazz.com>
2019-12-09 16:59:55 -0500

    add the srctree Kbuild var to kmaxall
    this is needed for kbuild files that use the srctree variable, e.g., arch/mips/Kbuild.platforms

--- scripts/kmaxlinux.sh
+++ scripts/kmaxlinux.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+kmax --version
+
 # script_dir=$(dirname $0)
 # makefile_override="$script_dir/makefile_override"
 # mkdir -p .kmax/topleveldirs
@@ -9,6 +11,5 @@ set -x
 #   echo -n "arch/$arch " > .kmax/topleveldirs/$arch
 #   timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null >> .kmax/topleveldirs/$arch
 # done
-# /usr/bin/time kmaxall -z $(cat .kmax/topleveldirs/* | tr ' ' '\n' | sort | uniq) > .kmax/kmax
-kmax --version
+# /usr/bin/time kmaxall -z $(cat .kmax/topleveldirs/* | tr ' ' '\n' | sort | uniq) $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt  > .kmax/kmax
 /usr/bin/time kmaxall -z $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt  > .kmax/kmax
