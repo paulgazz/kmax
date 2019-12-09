e0189aa48b3a60d72b869c66ebc1cbc4ef95ca32
Paul <github@pgazz.com>
2019-12-09 01:47:01 -0500

    add script to generate all formulas for the current version

---
+++ scripts/formulas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+scripts_dir=$(dirname $0)
+
+git describe
+if [[ ${?} == 0 ]]; then
+  rm -rf .kmax/
+  mkdir .kmax/
+  klocalizer --version >> .kmax/info.txt
+  echo "Linux git describe" >> .kmax/info.txt
+  date >> .kmax/info.txt
+  /usr/bin/time bash ${scripts_dir}/kmaxlinux.sh |& tee kmaxlinux.out
+  /usr/bin/time bash ${scripts_dir}/kclauselinux.sh |& tee kclauselinux.out
+else
+  echo "ERROR: Please run from top-level of linux repo." >&2
+  exit 1
+fi
