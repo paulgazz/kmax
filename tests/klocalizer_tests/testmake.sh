bf47c4c59c5c9338fc9eca3abcf634fae68e49da
Paul <github@pgazz.com>
2019-12-10 20:46:17 -0500

    factor out script that tests klocalizer and use it

--- tests/klocalizer_tests/testmake.sh
+++ tests/klocalizer_tests/testmake.sh
@@ -2,89 +2,31 @@
 
 set -x
 
-script_dir="$(dirname $0)/../../scripts/"
-
-# USAGE: test_klocalizer unit_name build_path kbuild_path
-test_klocalizer () {
-  filename=$1
-  if [[ "${#}" > "1" ]]; then
-    target="${2}"
-  else
-    target="$filename"
-    # target="$(dirname $filename)/"
-  fi
-  if [[ "${#}" > "2" ]]; then
-      kbuild_path="${3}"
-  else
-    kbuild_path="$filename"
-  fi
-
-  echo "$filename"
-  /usr/bin/time klocalizer "${filename}" > archname.txt
-  errcodekloc=${?}
-  cat archname.txt
-  if [[ $errcodekloc -eq 4 ]]; then
-        grep "${kbuild_path}" archname.txt
-        if [[ "${?}" -eq 0 ]]; then
-              /usr/bin/time klocalizer "${kbuild_path}" > archname.txt
-              errcodekloc=${?}
-        fi
-  fi
-  if [[ ${errcodekloc} -eq 0 ]]; then
-      arch=$(cat archname.txt)
-      if [[ "${arch}" == "um32" ]]; then
-          archvar="ARCH=um SUBARCH=i386"
-      else
-          archvar="ARCH=$(cat archname.txt)"
-      fi
-      "${script_dir}/make.cross" $archvar olddefconfig > build.txt 2>&1
-      "${script_dir}/make.cross" $archvar clean >> build.txt 2>&1
-      "${script_dir}/make.cross" $archvar "$target" >> build.txt 2>&1
-      errcodemake=${?}
-      tail -n1000 build.txt
-      if [[ ${errcodemake} -eq 0 ]]; then
-          grep "CC *$kbuild_path" build.txt
-          errcodegrep=${?}
-          if [[ ${errcodegrep} -eq 0 ]]; then
-              echo "PASS all $filename"
-          else
-            echo "FAIL make.cross_missing $filename"
-          fi
-      else
-        echo "FAIL make.cross_error $filename"
-      fi
-  elif [[ ${errcodekloc} -eq 4 ]]; then
-    echo "NONE kbuild_paths need to pass a full kbuild path"
-  elif [[ ${errcodekloc} -eq 10 ]]; then
-    echo "NONE CONFIG_BROKEN $filename"
-  else
-    echo "FAIL klocalizer $filename"
-  fi
-}
+test_klocalizer="bash $(dirname $0)/../../scripts/test_klocalizer.sh"
 
 kmlocalizer --version
 
-test_klocalizer drivers/usb/storage/alauda.o
-test_klocalizer sound/soc/intel/boards/glk_rt5682_max98357a.o
-test_klocalizer sound/mips/sgio2audio.o
-test_klocalizer drivers/char/efirtc.o
-test_klocalizer sound/soc/mediatek/common/mtk-btcvsd.o
-test_klocalizer drivers/watchdog/pnx833x_wdt.o
-test_klocalizer drivers/tty/n_r3964.o
-test_klocalizer drivers/block/ataflop.o
-test_klocalizer drivers/char/ipmi/ipmi_devintf.o drivers/char/ipmi/
-test_klocalizer virt/kvm/arm/arm.o arch/arm/kvm/ arch/arm/kvm/../../../virt/kvm/arm/arm.o
-test_klocalizer drivers/watchdog/pcwd.o
-test_klocalizer drivers/watchdog/mixcomwd.o
-test_klocalizer virt/kvm/kvm_main.o arch/s390/kvm/ arch/s390/kvm/../../../virt/kvm/kvm_main.o
-test_klocalizer virt/kvm/kvm_main.o arch/mips/kvm/ arch/mips/kvm/../../../virt/kvm/kvm_main.o
-test_klocalizer drivers/block/amiflop.o
-test_klocalizer drivers/video/fbdev/fsl-diu-fb.o
-test_klocalizer drivers/watchdog/cpwd.o
-test_klocalizer drivers/watchdog/riowd.o
-test_klocalizer drivers/gpu/drm/i915/gem/i915_gem_context.o
-test_klocalizer arch/x86/kernel/irq_64.o
-test_klocalizer arch/x86/kernel/irq_32.o
-test_klocalizer arch/x86/um/signal.o
-test_klocalizer arch/x86/um/ptrace_64.o
-test_klocalizer arch/x86/um/ptrace_32.o
+${test_klocalizer} drivers/usb/storage/alauda.o
+${test_klocalizer} sound/soc/intel/boards/glk_rt5682_max98357a.o
+${test_klocalizer} sound/mips/sgio2audio.o
+${test_klocalizer} drivers/char/efirtc.o
+${test_klocalizer} sound/soc/mediatek/common/mtk-btcvsd.o
+${test_klocalizer} drivers/watchdog/pnx833x_wdt.o
+${test_klocalizer} drivers/tty/n_r3964.o
+${test_klocalizer} drivers/block/ataflop.o
+${test_klocalizer} drivers/char/ipmi/ipmi_devintf.o drivers/char/ipmi/
+${test_klocalizer} virt/kvm/arm/arm.o arch/arm/kvm/ arch/arm/kvm/../../../virt/kvm/arm/arm.o
+${test_klocalizer} drivers/watchdog/pcwd.o
+${test_klocalizer} drivers/watchdog/mixcomwd.o
+${test_klocalizer} virt/kvm/kvm_main.o arch/s390/kvm/ arch/s390/kvm/../../../virt/kvm/kvm_main.o
+${test_klocalizer} virt/kvm/kvm_main.o arch/mips/kvm/ arch/mips/kvm/../../../virt/kvm/kvm_main.o
+${test_klocalizer} drivers/block/amiflop.o
+${test_klocalizer} drivers/video/fbdev/fsl-diu-fb.o
+${test_klocalizer} drivers/watchdog/cpwd.o
+${test_klocalizer} drivers/watchdog/riowd.o
+${test_klocalizer} drivers/gpu/drm/i915/gem/i915_gem_context.o
+${test_klocalizer} arch/x86/kernel/irq_64.o
+${test_klocalizer} arch/x86/kernel/irq_32.o
+${test_klocalizer} arch/x86/um/signal.o
+${test_klocalizer} arch/x86/um/ptrace_64.o
+${test_klocalizer} arch/x86/um/ptrace_32.o
