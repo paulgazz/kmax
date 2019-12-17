b343e17cf214919e51621797962e59d9cb728068
Paul <github@pgazz.com>
2019-12-17 16:24:31 -0500

    updated to allow for custom version name

--- scripts/formulas.sh
+++ scripts/formulas.sh
@@ -4,16 +4,21 @@ set -x
 
 scripts_dir=$(dirname $0)
 
-git describe
-if [[ ${?} == 0 ]]; then
-  rm -rf .kmax/
-  mkdir .kmax/
-  klocalizer --version >> .kmax/info.txt
-  echo "Linux $(git describe)" >> .kmax/info.txt
-  date >> .kmax/info.txt
-  /usr/bin/time bash ${scripts_dir}/kmaxlinux.sh |& tee kmaxlinux.out
-  /usr/bin/time bash ${scripts_dir}/kclauselinux.sh |& tee kclauselinux.out
+if [ "${#}" -gt 0 ]; then
+  version="${1}"
 else
-  echo "ERROR: Please run from top-level of linux repo." >&2
-  exit 1
+  version=$(git describe)
+  if [ "${?}" -ne 0 ]; then
+    echo "could not find git tag"
+    exit 1
+  fi
 fi
+
+rm -rf .kmax/
+mkdir .kmax/
+klocalizer --version >> .kmax/info.txt
+echo "Linux ${version}" >> .kmax/info.txt
+date >> .kmax/info.txt
+/usr/bin/time bash ${scripts_dir}/kmaxlinux.sh |& tee kmaxlinux.out
+/usr/bin/time bash ${scripts_dir}/kclauselinux.sh |& tee kclauselinux.out
+tar -jcvf "kmax-formulas_linux-${version}.tar.bz2" .kmax/
