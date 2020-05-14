0b3bffbb1978a0baa2e86423b6af175d9fa5b361
Paul <github@pgazz.com>
2020-05-14 09:25:27 -0400

    Complete rewrite of expression converter using python 3's ast module
    The expression converter takes an expression extracted from Kconfig
    and builds a z3 expression from it.  Previously, it used python 2's
    built-in support for parsing and compilation of python to parse and
    visit the expression tree.  python 3 provides a new module called ast
    that has similar functionality, but a different api.

--- kmaxtools/expression_converter.py
+++ kmaxtools/expression_converter.py
@@ -1,6 +1,6 @@
 import traceback
 import sys
-import compiler
+import ast
 import z3
 import regex
 
@@ -27,176 +27,200 @@ def glean_unknown_symbol(sym):
     return z3.StringVal(sym)
 
 # parse tree processing
-class Transformer(compiler.visitor.ASTVisitor):
+class Converter(ast.NodeVisitor):
   def __init__(self):
-    compiler.visitor.ASTVisitor.__init__(self)
-    self.tree = None
+    ast.NodeVisitor.__init__(self)
+    self.z3 = None
 
-  def default(self, node):
-    # sys.stderr.write("trying to process default: \"%s\"\n" % (node))
-    result = glean_unknown_symbol(node)
-    # sys.stderr.write("result: %s\n" % (result))
-    if result is not None:
-      return result
+  def result(self):
+    return self.z3
+
+  def visit_Expr(self, node):
+    self.generic_visit(node)
+    node.z3 = node.value.z3
+    self.z3 = node.z3
+
+  def visit_BoolOp(self, node):
+    self.generic_visit(node)
+    operands = []
+    for value in node.values:
+      operands.append(value.z3)
+    # string values should not normally appear in boolean expressions,
+    # but this can happen when there are undefined configuration
+    # options used in expressions.  instead of throwing an error,
+    # kconfig treats the usage of undefined configuration options
+    # (identifiers) as strings.  such strings are assumed to be false,
+    # since they aren't options that can ever be true.  (see visit_UnaryOp)
+    operands = [(operand if z3.is_bool(operand) else z3.BoolVal(False)) for operand in operands]
+    if node.op.name == "and":
+      node.z3 = z3.And(operands)
+    elif node.op.name == "or":
+      node.z3 = z3.Or(operands)
     else:
-      predicate = str(node)
-      return z3.Bool("PREDICATE_%s" % (predicate))
+      assert(False)
 
-  def visitCompare(self, node):
-    children = node.getChildren()
-    if len(children) == 3:
-      left = node.getChildren()[0]
-      op = str(node.getChildren()[1])
-      if op == "==" or op == "!=":
-        right = node.getChildren()[2]
-        left_z3 = self.visit(left)
-        right_z3 = self.visit(right)
-        if z3.is_string(left_z3) and z3.is_string(right_z3) or z3.is_bool(left_z3) and z3.is_bool(right_z3):
-          if op == "==":
-            result = z3.simplify(z3.Not(z3.Distinct(left_z3, right_z3)))
-            return result
-          elif op == "!=":
-            # sys.stderr.write("%s != %s\n" % (left_z3, right_z3))
-            result = z3.simplify(z3.Distinct(left_z3, right_z3))
-            return result
-        # elif z3.is_string(left_z3) and z3.is_bool(right_z3):
-        #   if op == "==":
-        #     pass
-        #   elif op == "!=":
-        #     pass
-        # elif z3.is_bool(left_z3) and z3.is_string(right_z3):
-        #   if op == "==":
-        #     pass
-        #   elif op == "!=":
-        #     pass
-          
-    # this expression is not supported, so make a predicate variable for it
-    predicate = str(node)
-    return z3.Bool("PREDICATE_%s" % (predicate))
+  def visit_And(self, node):
+    node.name = "and"
 
-  def visitDiscard(self, node):
-    # sys.stderr.write("%s\n" % (node.getChildren()[0]))
-    self.tree = self.visit(node.getChildren()[0])
-    # sys.stderr.write("%s\n" % (self.tree))
+  def visit_Or(self, node):
+    node.name = "or"
 
-  def visitOr(self, node):
-    # sys.stderr.write("or %s\n" % (str(node)))
-    children = node.getChildren()
-    # sys.stderr.write("%s\n" % str(children))
-    operands = [self.visit(child) for child in children]
-    # sys.stderr.write("%s\n" % str(operands))
-    # for child in operands:
-    #   sys.stderr.write("%s\n" % str(child))
-    #   sys.stderr.write("%s\n" % str(z3.is_bool(child)))
-    operands = [(child if z3.is_bool(child) else z3.BoolVal(False)) for child in operands]
-    return z3.Or(operands)
+  def visit_Not(self, node):
+    node.name = "not"
 
-  def visitAnd(self, node):
-    # sys.stderr.write("and %s\n" % (str(node)))
-    children = node.getChildren()
-    # sys.stderr.write("%s\n" % str(children))
-    operands = [self.visit(child) for child in children]
-    # sys.stderr.write("%s\n" % str(operands))
-    # for child in operands:
-    #   sys.stderr.write("%s\n" % str(child))
-    #   sys.stderr.write("%s\n" % str(z3.is_bool(child)))
-    operands = [(child if z3.is_bool(child) else z3.BoolVal(False)) for child in operands]
-    return z3.And(operands)
-
-  def visitNot(self, node):
-    # sys.stderr.write("not %s\n" % (str(node)))
-    operand = self.visit(node.expr)
-    if z3.is_bool(operand):
-      return z3.Not(operand)
+  def visit_UnaryOp(self, node):
+    self.generic_visit(node)
+    operand = node.operand.z3
+    # print("lalalal")
+    # print(operand)
+    if node.op.name == "not":
+      if z3.is_bool(operand):
+        node.z3 = z3.Not(operand)
+      else:
+        # the expression could be a stringval in the case of the usage
+        # of an undefined configuration option, which kconfig allows.
+        # we treat such variables are always off, so negating that
+        # variable makes it always true.  we can't assume all strings
+        # are undefined variables, because string constants are also
+        # permitted in kconfig.  such string constants have a false
+        # value anyway.  (see visit_BoolOp)
+        node.z3 = z3.BoolVal(True)
     else:
-      return z3.BoolVal(True)
+      assert(False)
 
-  def visitName(self, node):
-    # sys.stderr.write("name %s\n" % (str(node)))
-    result = z3.Bool(node.name)
-    # sys.stderr.write("name z3 %s\n" % (str(result)))
-    # sys.stderr.write("name is_bool %s\n" % (str(z3.is_bool(result))))
-    return result
-
-  def visitConst(self, node):
+  def visit_Constant(self, node):
     # sys.stderr.write("const %s %s\n" % (str(node), str(type(node.value))))
     value = node.value
     result = glean_unknown_symbol(value)
     # sys.stderr.write("result const: %s\n" % (result))
     if result is not None:
-      return result
+      node.z3 = result
     else:
       print(traceback.format_exc())
       sys.stderr.write("error: cannot process value \"%s\"\n" % (value))
       exit(1)
-      return None
+      node.z3 = None
+
+  def visit_Name(self, node):
+    # sys.stderr.write("name %s\n" % (str(node)))
+    node.z3 = z3.Bool(node.id)
+    # sys.stderr.write("name z3 %s\n" % (str(result)))
+    # sys.stderr.write("name is_bool %s\n" % (str(z3.is_bool(result))))
+    
+  def visit_Compare(self, node):
+    self.generic_visit(node)
+    if len(node.ops) > 1:
+      # kconfig constraints aren't expected to have multiple operators in a comparison expression
+      assert(False)
+    left = node.left.z3
+    op = node.ops[0].name
+    right = node.comparators[0].z3
+
+    # model non-Boolean operators by creating a predicate variable.
+    # TODO: model relational expressions and expressions between variables and strings in Z3
+    predicate = str(ast.dump(node))
+    node.z3 = z3.Bool("PREDICATE_%s" % (predicate))
+
+    # represent (in)equality between booleans and strings using z3 expressions
+    if z3.is_string(left) and z3.is_string(right) or z3.is_bool(left) and z3.is_bool(right):
+      if op == "eq":
+        node.z3 = z3.simplify(z3.Not(z3.Distinct(left, right)))
+      elif op == "ne":
+        node.z3 = z3.simplify(z3.Distinct(left, right))
+      # elif op == "lt":
+      #   node.z3 = z3.Z3_mk_lt(z3.get_ctx(None), left.ast, right.ast)
+      # elif op == "le":
+      #   node.z3 = z3.simplify(z3.ULE(left, right))
+      # elif op == "gt":
+      #   node.z3 = z3.simplify(z3.UGT(left, right))
+      # elif op == "ge":
+      #   node.z3 = z3.simplify(z3.UGE(left, right))
+      # else:
+      #  assert(False)
+
+  def visit_Eq(self, node):
+    node.name = "eq"
+
+  def visit_NotEq(self, node):
+    node.name = "ne"
+
+  def visit_Lt(self, node):
+    node.name = "lt"
+
+  def visit_LtE(self, node):
+    node.name = "lte"
+
+  def visit_Gt(self, node):
+    node.name = "gt"
+
+  def visit_GtE(self, node):
+    node.name = "gte"
+
+  # def default(self, node):
+  #   # sys.stderr.write("trying to process default: \"%s\"\n" % (node))
+  #   result = glean_unknown_symbol(node)
+  #   # sys.stderr.write("result: %s\n" % (result))
+  #   if result is not None:
+  #     return result
+  #   else:
+  #     predicate = str(node)
+  #     return z3.Bool("PREDICATE_%s" % (predicate))
+
 
 # parse tree processing
-class IdentifierCollector(compiler.visitor.ASTVisitor):
+class IdentifierCollector(ast.NodeVisitor):
   def __init__(self):
-    compiler.visitor.ASTVisitor.__init__(self)
-    self.tree = ()
+    self.identifiers = []
 
-  def default(self, node):
-    predicate = str(node)
-    return ["PREDICATE_%s" % (predicate)]
+  def result(self):
+    return self.identifiers
 
-  def visitDiscard(self, node):
-    self.tree = self.visit(node.getChildren()[0])
-
-  def visitOr(self, node):
-    children = node.getChildren()
-    left = []
-    for i in range(0, len(children)):
-      left.extend(self.visit(children[i]))
-    return left
-
-  def visitAnd(self, node):
-    children = node.getChildren()
-    left = []
-    for i in range(0, len(children)):
-      left.extend(self.visit(children[i]))
-    return left
-
-  def visitNot(self, node):
-    return self.visit(node.expr)
-
-  def visitName(self, node):
-    return [node.name]
-
-  def visitConst(self, node):
-    return []
+  def visit_Name(self, node):
+    self.identifiers.append(node.id)
 
 def get_identifiers(expr):
   try:
-    ast = compiler.parse(expr)
+    tree = ast.parse(expr)
   except:
     sys.stderr.write("error: could not parse %s\n" % (line))
     sys.stderr.write(traceback.format_exc())
     sys.stderr.write("\n")
     return []
-  actual_expr = ast.getChildNodes()[0].getChildNodes()[0]
-  transformer = IdentifierCollector()
-  compiler.walk(actual_expr, transformer, walker=transformer, verbose=True)
-  return transformer.tree
+  visitor = IdentifierCollector()
+  visitor.visit(tree)
+  return visitor.result()
 
 def convert_to_z3(expr):
+  # print("JFLDSJFKLSDJKL")
+  # print(expr)
   try:
-    ast = compiler.parse(expr)
+    tree = ast.parse(expr)
   except RuntimeError as e:
     sys.stderr.write("exception: %s\n" % (e))
     sys.stderr.write("could not convert expression to cnf\n%s\n" % (expr))
     exit(1)
     return []
-  except:
+  except Error as e:
     # handle broken expressions
+    sys.stderr.write("exception: %s\n" % (e))
     sys.stderr.write("warning: couldn't parse %s\n" % (expr))
     exit(1)
     return []
-  actual_expr = ast.getChildNodes()[0].getChildNodes()[0]
-  transformer = Transformer()
   # sys.stderr.write("actual_expr %s\n" % (str(actual_expr)))
-  compiler.walk(actual_expr, transformer, walker=transformer, verbose=True)
-  z3_clause = transformer.tree
+  visitor = Converter()
+  # print(ast.dump(tree))
+  visitor.visit(tree)
+  z3_clause = visitor.result()
   # sys.stderr.write("z3_clause %s\n" % (str(z3_clause)))
   return z3.simplify(z3_clause)
+
+if __name__ == '__main__':
+  # tests
+  print(convert_to_z3('((((not ((((CONFIG_GCOV_KERNEL and CONFIG_CC_IS_GCC and 1)) and (((1 and CONFIG_GCC_VERSION < "40700")) or ((1)))))) or (CONFIG_GCOV_FORMAT_3_4 or CONFIG_GCOV_FORMAT_4_7))) and (((not (CONFIG_GCOV_FORMAT_3_4 or CONFIG_GCOV_FORMAT_4_7)) or ((CONFIG_GCOV_KERNEL and CONFIG_CC_IS_GCC and 1)))))'))
+  # print(convert_to_z3('((((not ((((1)) and (((1)) or ((1 and CONFIG_GCC_PLUGINS)) or ((1 and CONFIG_GCC_PLUGINS and ( not CONFIG_KASAN or CONFIG_KASAN_STACK!="1"))) or ((1 and CONFIG_GCC_PLUGINS and ( not CONFIG_KASAN or CONFIG_KASAN_STACK!="1"))) or ((1 and CONFIG_CC_HAS_AUTO_VAR_INIT)))))) or (CONFIG_INIT_STACK_NONE or CONFIG_GCC_PLUGIN_STRUCTLEAK_USER or CONFIG_GCC_PLUGIN_STRUCTLEAK_BYREF or CONFIG_GCC_PLUGIN_STRUCTLEAK_BYREF_ALL or CONFIG_INIT_STACK_ALL))) and (((not (CONFIG_INIT_STACK_NONE or CONFIG_GCC_PLUGIN_STRUCTLEAK_USER or CONFIG_GCC_PLUGIN_STRUCTLEAK_BYREF or CONFIG_GCC_PLUGIN_STRUCTLEAK_BYREF_ALL or CONFIG_INIT_STACK_ALL)) or ((1)))))'))
+  # print(convert_to_z3('((((not ((((1)) and (((1 and CONFIG_X86_64)) or ((1)) or ((1 and CONFIG_EXPERT and  not CONFIG_STACKDEPOT)))))) or (CONFIG_UNWINDER_ORC or CONFIG_UNWINDER_FRAME_POINTER or CONFIG_UNWINDER_GUESS))) and (((not (CONFIG_UNWINDER_ORC or CONFIG_UNWINDER_FRAME_POINTER or CONFIG_UNWINDER_GUESS)) or ((1)))))'))
+  # print(get_identifiers('((((not ((((1)) and (((1 and CONFIG_X86_64)) or ((1)) or ((1 and CONFIG_EXPERT and  not CONFIG_STACKDEPOT)))))) or (CONFIG_UNWINDER_ORC or CONFIG_UNWINDER_FRAME_POINTER or CONFIG_UNWINDER_GUESS))) and (((not (CONFIG_UNWINDER_ORC or CONFIG_UNWINDER_FRAME_POINTER or CONFIG_UNWINDER_GUESS)) or ((1)))))'))
+
+
+
+  
