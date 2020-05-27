44f2fd02001c911a5f58378615d041962a092e93
Paul <github@pgazz.com>
2020-05-27 00:07:03 -0400

    Added a version of kclause formula collection for Linux v3.19

---
+++ scripts/kclauselinux-3.19.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+script_dir=$(dirname $0)
+kclause --version
+for arch in x86_64 i386 arm arm64 sparc sparc64 mips ia64 powerpc alpha arc avr32 blackfin c6x cris frv hexagon m32r m68k metag microblaze mn10300 nios2 openrisc parisc s390 score sh sparc tile um unicore32 xtensa; do
+  if [[ "$arch" == "x86_64" || "$arch" == "i386" ]]; then
+    srcarch="x86"
+  elif [[ "$arch" == "sparc64" || "$arch" == "sparc" ]]; then
+    srcarch="sparc"
+  elif [[ "$arch" == "sh64" || "$arch" == "sh" ]]; then
+    srcarch="sh"
+  else
+    srcarch="$arch"
+  fi
+  make ARCH=$arch defconfig
+  mkdir -p .kmax/kclause/$arch
+  kextract --module-version 3.19 $arch .kmax/kclause/$arch/kextract
+  /usr/bin/time kclause --remove-orphaned-nonvisible < .kmax/kclause/$arch/kextract > .kmax/kclause/$arch/kclause
+done
