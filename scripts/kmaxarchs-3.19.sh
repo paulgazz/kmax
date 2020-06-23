96f799b0c6508aed5a86c36dbbd8512c953990b2
Paul <github@pgazz.com>
2020-06-23 17:27:07 -0400

    Scripts to collect compilation units for FSE repetition

---
+++ scripts/kmaxarchs-3.19.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run kmax once for architecture, using the unselectable config options to rule out arch-specific files
+
+for dir in .kmax/kclause/*; do
+  echo "processing ${dir}"
+  arch="$(basename ${dir})"
+  unselectable="${dir}/unselectable"
+  if [[ ! -f "${unselectable}" ]]; then
+    echo "\"${unselectable}\" not found.  please run selectable.sh first on the formulas."
+  else
+    if [[ "$arch" == "x86_64" || "$arch" == "i386" ]]; then
+      srcarch="x86"
+    elif [[ "$arch" == "sparc64" || "$arch" == "sparc" ]]; then
+      srcarch="sparc"
+    elif [[ "$arch" == "sh64" || "$arch" == "sh" ]]; then
+      srcarch="sh"
+    else
+      srcarch="$arch"
+    fi
+    if [[ "$arch" == "blackfin" ]]; then
+      extra_args=" -DARCH=blackfin "
+    elif [[ "$arch" == "um" ]]; then
+      extra_args=" -DOS=Linux "
+    fi
+    outfile="${dir}/units"
+    # linux 3.19
+    /usr/bin/time kmaxall --output-all-unit-types --unselectable "${unselectable}" arch/${srcarch} block crypto drivers fs init ipc kernel lib mm net samples security sound usr virt > "${outfile}.pending" 2> "${outfile}.err"
+    mv "${outfile}.pending" "${outfile}"
+  fi
+done
---
+++ scripts/kmaxlinuxunits.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+kmax --version
+
+# script_dir=$(dirname $0)
+# makefile_override="$script_dir/makefile_override"
+# mkdir -p .kmax/topleveldirs
+# for arch in $(ls arch/ | egrep -v ".gitignore|Kconfig"); do
+#   echo -n "arch/$arch " > .kmax/topleveldirs/$arch
+#   timeout 4 make ARCH=$arch -f "$makefile_override" alldirs 2>/dev/null >> .kmax/topleveldirs/$arch
+# done
+# /usr/bin/time kmaxall -a -z $(cat .kmax/topleveldirs/* | tr ' ' '\n' | sort | uniq) $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt  > .kmax/units_override
+
+/usr/bin/time kmaxall -a $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt > .kmax/units.pending
+mv .kmax/units.pending .kmax/units
