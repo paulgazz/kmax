e14a285c090e01dd066dd455209872d98e256f35
Paul <github@pgazz.com>
2020-06-23 23:54:41 -0400

    Script the creation of a single kmax file for Linux 3.19
    Two architectures requires non-Boolean variables to be set,
    ARCH=blackfin for blackfin and OS=Linux for um.

--- scripts/kmaxlinuxunits-3.19.sh
+++ scripts/kmaxlinuxunits-3.19.sh
@@ -2,7 +2,16 @@
 
 set -x
 
-kmax --version
+kmaxall --version
 
-/usr/bin/time kmaxall -a $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt > .kmax/units.pending
-mv .kmax/units.pending .kmax/units
+# # remove prereq usage in arch/arm/Makefile and arch/mips/Makefile to get top-level directories
+# /usr/bin/time kmaxall -a $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt > .kmax/units.pending1
+
+# add blackfin results which need ARCH set to get top-level directories
+/usr/bin/time kmaxall -F .kmax/units.pending1 -a -DARCH=blackfin arch/blackfin/Makefile > .kmax/units.pending2
+
+# TODO: replace shell call to define OS with Linux in arch/um/Makefile
+# add um results which need OS set and a call to shell replaced to get top-level directories
+/usr/bin/time kmaxall -F .kmax/units.pending2 -DOS=Linux -a arch/um/ > .kmax/units.pending3
+
+cp .kmax/units.pending3 .kmax/units
