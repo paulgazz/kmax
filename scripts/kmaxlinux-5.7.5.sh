7fb791f13f5c3a09719b35482ff1a698219b4055
Paul <github@pgazz.com>
2020-06-27 04:39:15 -0400

    Added scripts to run kmax for Linux 5.7.5
    Includes settings of non-Boolean variables from the command-line for
    particular subdirectories.

---
+++ scripts/kmaxlinux-5.7.5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+kmaxall --version
+
+# remove prereq usage in arch/arm/Makefile and arch/mips/Makefile to get top-level directories
+/usr/bin/time kmaxall $(find arch/ -maxdepth 1 -mindepth 1 | egrep -v ".gitignore|Kconfig") block certs crypto drivers fs init ipc kernel lib mm net samples security sound usr virt > .kmax/kmax.pending1
+
+# add archs that require ARCH to be set to get top-level directories
+/usr/bin/time kmaxall -F .kmax/kmax.pending1 -DARCH=h8300 arch/h8300/Makefile > .kmax/kmax.pending2
+/usr/bin/time kmaxall -F .kmax/kmax.pending2 -DARCH=mips arch/mips/Makefile > .kmax/kmax.pending3
+
+# amd includes Makefiles explicitly instead of using Kbuild
+/usr/bin/time kmaxall -F .kmax/kmax.pending3 -DFULL_AMD_DISPLAY_PATH=drivers/gpu/drm/amd/display -DFULL_AMD_PATH=drivers/gpu/drm/amd -DDISPLAY_FOLDER_NAME=display drivers/gpu/drm > .kmax/kmax.pending4
+
+# TODO: replace shell call to define OS with Linux in arch/um/Makefile
+# add um results which need OS set and a call to shell replaced to get top-level directories
+/usr/bin/time kmaxall -F .kmax/kmax.pending4 -DOS=Linux arch/um/ > .kmax/kmax.pending5
+
+cp .kmax/kmax.pending5 .kmax/kmax
