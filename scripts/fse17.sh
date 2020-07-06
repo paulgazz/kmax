f11f5fca51e7ed350d33473d16f5de0b725ffe2a
Paul <github@pgazz.com>
2020-07-05 22:18:57 -0400

    No longer need per-version kmax collection scripts

--- scripts/fse17.sh
+++ scripts/fse17.sh
@@ -9,5 +9,5 @@ kmax --version >> .kmax/info.txt
 date >> .kmax/info.txt
 /usr/bin/time bash ${scripts_dir}/kclauselinux-3.19.sh
 /usr/bin/time bash ${scripts_dir}/unselectable_options.sh
-/usr/bin/time bash ${scripts_dir}/kmaxarchs-3.19.sh
-/usr/bin/time bash ${scripts_dir}/completeness.py
+/usr/bin/time bash ${scripts_dir}/kmaxarchs.sh
+/usr/bin/time bash ${scripts_dir}/completeness.py 3.19 '.kmax/archs/units.*'
