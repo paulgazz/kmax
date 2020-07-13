a74c47178ca371eee66f865fa4c03d3250aab1f1
Paul <github@pgazz.com>
2020-07-13 18:26:22 -0400

    Separate out running kextract on Linux from running kclause
    With this change, only the script to run kextract needs differ between
    Linux versions, since kextract produces the same format for kclause to
    use on all.

--- scripts/kclauselinux.sh
+++ scripts/kclauselinux.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-set -x
-
-script_dir=$(dirname $0)
-kclause --version
-for arch in x86_64 i386 arm arm64 sparc sparc64 mips ia64 powerpc alpha arc c6x csky h8300 hexagon m68k microblaze nds32 nios2 openrisc parisc riscv s390 sh sh64 unicore32 xtensa; do
-  if [[ "$arch" == "x86_64" || "$arch" == "i386" ]]; then
-    srcarch="x86"
-  elif [[ "$arch" == "sparc64" || "$arch" == "sparc" ]]; then
-    srcarch="sparc"
-  elif [[ "$arch" == "sh64" || "$arch" == "sh" ]]; then
-    srcarch="sh"
-  else
-    srcarch="$arch"
-  fi
-  make ARCH=$arch defconfig
-  mkdir -p .kmax/kclause/$arch
-  kextract $arch .kmax/kclause/$arch/kextract
-  /usr/bin/time kclause --remove-orphaned-nonvisible < .kmax/kclause/$arch/kextract > .kmax/kclause/$arch/kclause
+for dir in .kmax/kclause/*; do
+  echo "processing $dir"
+  /usr/bin/time kclause --remove-orphaned-nonvisible < "${dir}/kextract" > ."${dir}/kclause.pending"
+  mv "${dir}/kclause.pending" "${dir}/kclause"
 done
--- scripts/kextractlinux.sh
+++ scripts/kextractlinux.sh
@@ -4,7 +4,7 @@ set -x
 
 script_dir=$(dirname $0)
 kclause --version
-for arch in x86_64 i386 arm arm64 sparc sparc64 mips ia64 powerpc alpha arc avr32 blackfin c6x cris frv hexagon m32r m68k metag microblaze mn10300 nios2 openrisc parisc s390 score sh sparc tile um unicore32 xtensa; do
+for arch in x86_64 i386 arm arm64 sparc sparc64 mips ia64 powerpc alpha arc c6x csky h8300 hexagon m68k microblaze nds32 nios2 openrisc parisc riscv s390 sh sh64 unicore32 xtensa; do
   if [[ "$arch" == "x86_64" || "$arch" == "i386" ]]; then
     srcarch="x86"
   elif [[ "$arch" == "sparc64" || "$arch" == "sparc" ]]; then
@@ -16,5 +16,5 @@ for arch in x86_64 i386 arm arm64 sparc sparc64 mips ia64 powerpc alpha arc avr3
   fi
   make ARCH=$arch defconfig
   mkdir -p .kmax/kclause/$arch
-  kextract --module-version 3.19 $arch .kmax/kclause/$arch/kextract
+  kextractlinux $arch .kmax/kclause/$arch/kextract
 done
