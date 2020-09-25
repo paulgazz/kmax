aa9a9db98712355867c271d93207506b8da5d61a
Necip Fazil Yildiran <fazilyildiran@gmail.com>
2020-09-25 13:52:13 +0300

    kclause: introduce composite format for klocalizer
    Currently, kclause files are dictionaries, which are needed to be parsed
    by klocalizer on each run such that each clause in the dictionary is
    accumulated to a constraints list. This process has a great share on
    the overall running time the kclause by almost doubling it.
    
    Create a new kclause format, i.e., composite kclause, which takes away
    the need for traversing a dictionary, and instead stores the composite
    clauses already accumuluated in a list.
    
    kmax/klocalizer now takes --use-composite-kclause-formulas-files flag,
    which tells klocalizer to interpret the kclause formulas file in composite
    format.
    
    scripts/kclause_to_composite.py can be used to convert kclause files into
    composite kclause files. The functionality of this script can be later
    embedded into the formula extractor/generator to have this seamless.

---
+++ scripts/kclause_to_composite.py
@@ -0,0 +1,39 @@
+import z3, pickle, os, glob, argparse
+
+parser = argparse.ArgumentParser(description="Create composite kclause formulas files from kclause files in place.")
+helptxt="The path to the .kmax folder in which the kclause formulas files are to be converted to composite kclause formulas files."
+parser.add_argument('-k', '--kmax-path', default=None, type=str, required=True, help=helptxt)
+kmax_path = parser.parse_args().kmax_path
+
+assert os.path.exists(kmax_path) and os.path.isdir(kmax_path)
+
+all_kclause_files = glob.glob(os.path.join(kmax_path, "kclause", "*", "kclause"))
+
+for f in all_kclause_files:
+    assert os.path.isfile(f)
+
+for kclause_file in all_kclause_files:
+    print("Processing %s" % kclause_file)
+    with open(kclause_file, 'rb') as fp:
+        kclause = pickle.load(fp)
+
+        kclause_constraints = {}
+        for var in kclause.keys():
+            kclause_constraints[var] = [ z3.parse_smt2_string(clause) for clause in kclause[var] ]
+
+        vec = z3.AstVector()
+        for var in kclause_constraints.keys():
+            for z3_clause in kclause_constraints[var]:
+                for cl in z3_clause:
+                    vec.push(cl)
+        
+        constraints = vec
+
+        solver = z3.Solver()
+        solver.add(constraints)
+
+        composite_kclause_file = kclause_file
+        with open(composite_kclause_file, 'w') as fp:
+            fp.write(solver.to_smt2())
+        print("Written the composite kclause file to '%s'" % composite_kclause_file)
+        
