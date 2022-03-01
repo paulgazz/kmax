eb48dde9d43650e57f046f14d507446130dd3edc
Necip Fazil Yildiran <fazilyildiran@gmail.com>
2022-02-28 19:59:57 -0500

    klocalizer: accept .patch/.diff files as input
    Now --include/--include-mutex/--exclude also take unified diff files as
    input.  If the argument to those options has .patch or .diff extension,
    it is taken to be a unified diff file.  The file is parsed to get a set
    of unit:line pairs for files with .c extension.

--- kmax/common.py
+++ kmax/common.py
@@ -1,6 +1,7 @@
 import sys
 import pickle
 import z3
+import enum
 
 quiet = False
 verbose = False
@@ -81,3 +82,56 @@ def unpickle_kmax_file(kmax_file):
   with open(kmax_file, 'rb') as fp:
     kmax = pickle.load(fp)
     return kmax
+
+class SourceFileType(enum.Enum):
+  SOURCE = 1 # .c
+  HEADER = 2 # .h
+  MAKEFILE = 3 # Makefile/Kbuild
+  KCONFIG = 4 # Kconfig
+  OTHER = 5 # anything else
+
+  @classmethod
+  def get_file_type(cls, filename: str):
+    assert filename != None
+
+    if filename.endswith('.c'):
+      return SourceFileType.SOURCE
+    elif filename.endswith('.h'):
+      return SourceFileType.HEADER
+    elif filename.startswith("Makefile") or filename.startswith("Kbuild"):
+      return SourceFileType.MAKEFILE
+    elif filename.startswith("Kconfig"):
+      return SourceFileType.KCONFIG
+    else: 
+      return SourceFileType.OTHER
+
+class FileChangeType(enum.Enum):
+	CREATED = 1
+	REMOVED = 2
+	MOVED_ONLY = 3 # file was moved but the content didn't change
+	MOVED_MODIFIED = 4 # file was moved and the content was modified
+	MODIFIED_ONLY = 5 # content was modified but the file wasn't moved
+
+	@classmethod
+	def getType(cls, diff):
+		none_file="/dev/null"
+		assert not (diff.header.old_path == none_file and diff.header.new_path == none_file)
+		
+		if diff.header.old_path == none_file:
+			return FileChangeType.CREATED
+		elif diff.header.new_path == none_file:
+			return FileChangeType.REMOVED
+		elif diff.header.old_path != diff.header.new_path:
+			# filename changed
+			if diff.changes:
+				return FileChangeType.MOVED_MODIFIED
+			else:
+				return FileChangeType.MOVED_ONLY
+		elif diff.header.old_path == diff.header.new_path and diff.changes:
+			return FileChangeType.MODIFIED_ONLY
+		elif "deleted file" in diff.text:
+			# When deleted a file, some patches might have the same file path for
+			# both before and after instead of none_file name. See
+			# e8bf1f522aee3b3e1e7658e8f224dca1d88c3338 for the Linux kernel.
+			return FileChangeType.REMOVED
+		else: assert False # all cases are covered above
