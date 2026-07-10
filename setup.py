5473c5b70a2b17eba9d5a08bda58764a7a08266d
Paul Gazzillo <paul@pgazz.com>
2026-07-10 00:17:32 -0400

    Remove dd and networkx version requirements
    These requirements were for python2, which is no longer supported.
    
    Moreover, this now breaks the build.  Fixes #306.

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup(
         'enum34',
         'regex',
         'z3-solver',
-        'dd==0.5.7', # dd requires networkx >= 2.4 starting from 0.6.0
-        'networkx==2.2', # for dd to work on python2
+        'dd',
+        'networkx',
         'requests',
         'whatthepatch',
         'packaging',
